a1fb159ba7aae7fcc3d2b386e6dc55b50f56f9ce
AlGore <algore>
2018-05-07 19:27:25 -0700

    Add configuration values in app.config / parsing logic

--- CryptoPrices/CryptoPrices/Program.cs
+++ CryptoPrices/CryptoPrices/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CryptoPrices.Utilities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 
 namespace CryptoPrices
 {
@@ -10,6 +14,18 @@ namespace CryptoPrices
     {
         static void Main(string[] args)
         {
+            var pairs = ConfigurationManager.AppSettings.AllKeys.Where(key => key.StartsWith("Pair")).Select(key => ConfigurationManager.AppSettings[key]).Select(values =>
+            {
+                var data = values.Split(',');
+                var coin = data[0];
+                var pair = data[1];
+                var exchange = data[2];
+                var entry = double.Parse(data[3]);
+                var target1 = double.Parse(data[4]);
+                var target2 = double.Parse(data[5]);
+                var stopLoss = double.Parse(data[6]);
+                return new Tuple<string, string, string, double, double, double, double>(coin, pair, exchange, entry, target1, target2, stopLoss);
+            });
         }
     }
 }
