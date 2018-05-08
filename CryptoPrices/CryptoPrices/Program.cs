8fad6e4c6d4ff10526939ef4d1d77bbece4e7fc6
AlGore <algore>
2018-05-07 20:12:40 -0700

    Add refresh rate and logging for TA

--- CryptoPrices/CryptoPrices/Program.cs
+++ CryptoPrices/CryptoPrices/Program.cs
@@ -1,12 +1,15 @@
 ﻿using CryptoPrices.Utilities;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 
 using System.Linq;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CryptoPrices
 {
@@ -14,7 +17,9 @@ namespace CryptoPrices
     {
         static void Main(string[] args)
         {
-            var pairs = ConfigurationManager.AppSettings.AllKeys.Where(key => key.StartsWith("Pair")).Select(key => ConfigurationManager.AppSettings[key]).Select(values =>
+            var refreshRateInSeconds = !int.TryParse(ConfigurationManager.AppSettings["RefreshRateInSeconds"], out int result) || result < 30 ? 30 : result;
+
+            var tradeSignals = ConfigurationManager.AppSettings.AllKeys.Where(key => key.StartsWith("TradeSignal")).Select(key => ConfigurationManager.AppSettings[key]).Select(values =>
             {
                 var data = values.Split(',');
                 var coin = data[0];
@@ -27,15 +32,34 @@ namespace CryptoPrices
                 return new Tuple<string, string, string, double, double, double, double>(coin, pair, exchange, entry, target1, target2, stopLoss);
             });
 
-
-            var bitcoinForks = ConfigurationManager.AppSettings.AllKeys.Where(key => key.StartsWith("Fork")).Select(key => ConfigurationManager.AppSettings[key]).Select(values =>
+            var logBuilder = new ConcurrentBag<string>();
+            while (true)
             {
-                var data = values.Split(',');
-                var coin = data[0];
-                var pair = "USD";
-                var multiplier = int.Parse(data[1]);
-                return new Tuple<string, string, int>(coin, pair, multiplier);
-            });
+                Parallel.ForEach(tradeSignals, new ParallelOptions() { MaxDegreeOfParallelism = 10 }, tradeSignal =>
+                {
+                    var coin = tradeSignal.Item1;
+                    var pair = tradeSignal.Item2;
+                    var exchange = tradeSignal.Item3;
+                    var entry = tradeSignal.Item4;
+                    var t1 = tradeSignal.Item5;
+                    var t2 = tradeSignal.Item6;
+                    var stop = tradeSignal.Item7;
+
+                    var coinPrice = API.GetPrices(coin, new[] { pair }, exchange);
+                    if (!coinPrice.ContainsKey(pair)) { return; }
+                    var curPrice = coinPrice[pair];
+                    var profitLoss = (1 - (entry / curPrice)) * 100;
+
+                    logBuilder.Add($"{coin}/{pair} ({exchange}) - Price: {curPrice.ToString("#.######")} || E: {entry.ToString("#.######")} | T1: {t1.ToString("#.######")} | T2: {t2.ToString("#.######")} | Stop: {stop.ToString("#.######")} || P/L: {profitLoss.ToString("###.#")}% ");
+                });
+                Console.Clear();
+                foreach (var log in logBuilder.OrderByDescending(e => e))
+                {
+                    Console.WriteLine(log);
+                }
+                logBuilder = new ConcurrentBag<string>();
+                Thread.Sleep(TimeSpan.FromSeconds(refreshRateInSeconds));
+            }
 
         }
     }
